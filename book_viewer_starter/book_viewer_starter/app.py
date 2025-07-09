803e23dfb388dd053561ae44359b11b220465f0b
Justin Schultz <SchultzJustin@ymail.com>
2025-07-09 22:57:31 +0000

    Add bolding to search queries, reorganize app.py for better readability

--- book_viewer_starter/book_viewer_starter/app.py
+++ book_viewer_starter/book_viewer_starter/app.py
@@ -1,33 +1,16 @@
 from flask import Flask, render_template, g, redirect, request
+import re
 
 app = Flask(__name__)
 
-@app.before_request
-def load_contents():
-    with open('book_viewer/data/toc.txt', 'r') as file:
-        g.contents = file.readlines()
+# Helper functions
+def bold(text, query):
+    pattern = re.compile(re.escape(query), re.IGNORECASE)
 
-@app.route('/')
-def index():
-    return render_template('home.html', contents=g.contents)
+    def replacer(match):
+        return f'<strong>{match.group(0)}</strong>'
 
-@app.route('/chapters/<page_num>')
-def chapter(page_num):
-    if page_num.isdigit() and (1 <= int(page_num) <= len(g.contents)):
-        chapter_name = g.contents[int(page_num) - 1]
-        chapter_title = f'Chapter {page_num}: {chapter_name}'
-
-        with open(f'book_viewer/data/chp{page_num}.txt') as file:
-            chapter_content = file.read()
-
-        paragraphs = chapter_content.split('\n\n')
-
-        return render_template('chapter.html',
-                                chapter_title=chapter_title,
-                                contents=g.contents,
-                                paragraphs=paragraphs)
-    else:
-        return render_template('redirect_countdown.html')
+    return pattern.sub(replacer, text)
 
 def in_paragraphs(text):
     paragraphs = text.split('\n\n')
@@ -62,6 +45,39 @@ def chapters_matching(query):
 
     return results
 
+# Register filters
+app.jinja_env.filters['in_paragraphs'] = in_paragraphs
+app.jinja_env.filters['bold'] = bold
+
+# Request hooks
+@app.before_request
+def load_contents():
+    with open('book_viewer/data/toc.txt', 'r') as file:
+        g.contents = file.readlines()
+
+# Route handlers
+@app.route('/')
+def index():
+    return render_template('home.html', contents=g.contents)
+
+@app.route('/chapters/<page_num>')
+def chapter(page_num):
+    if page_num.isdigit() and (1 <= int(page_num) <= len(g.contents)):
+        chapter_name = g.contents[int(page_num) - 1]
+        chapter_title = f'Chapter {page_num}: {chapter_name}'
+
+        with open(f'book_viewer/data/chp{page_num}.txt') as file:
+            chapter_content = file.read()
+
+        paragraphs = chapter_content.split('\n\n')
+
+        return render_template('chapter.html',
+                                chapter_title=chapter_title,
+                                contents=g.contents,
+                                paragraphs=paragraphs)
+    else:
+        return render_template('redirect_countdown.html')
+
 @app.route('/search')
 def search():
     query = request.args.get('query', '')
@@ -69,11 +85,10 @@ def search():
 
     return render_template('search.html', query=query, results=results)
 
+# Error handlers
 @app.errorhandler(404)
 def page_not_found(_error):
     return render_template('redirect_countdown.html'), 404
-
-app.jinja_env.filters['in_paragraphs'] = in_paragraphs
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
