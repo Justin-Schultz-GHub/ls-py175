7f1d4a0a9837bbc4271ef54a35fe0d3ebee8af24
Justin Schultz <SchultzJustin@ymail.com>
2025-07-10 22:38:46 +0000

    Extract total_interests to helper function, reformat for readability

--- user_tracker/app.py
+++ user_tracker/app.py
@@ -6,27 +6,36 @@ app = Flask(__name__)
 with open('users.yaml', 'r') as file:
     users = yaml.safe_load(file)
     total_users = len(users)
-    total_interests = 0
 
-    for value in users.values():
-        total_interests += len(value['interests'])
-
-    print(total_interests)
-    print(total_users)
+def total_interests(users):
+    return sum(len(user['interests']) for user in users.values())
 
 @app.route('/')
 def index():
-    return render_template('index.html', total_interests=total_interests, total_users= total_users)
+    return (render_template('index.html',
+            total_interests=total_interests(users),
+            total_users= total_users
+            ))
 
 @app.route('/users')
 def users_list():
-    return render_template('users.html', users=users, total_interests=total_interests, total_users= total_users)
+    return (render_template('users.html',
+            users=users,
+            total_interests=total_interests(users),
+            total_users= total_users
+            ))
 
 @app.route('/users/<user_name>')
 def user_profile(user_name):
     data = users.get(user_name)
     if data:
-        return render_template('user.html', users=users, data=data, user_name=user_name, total_interests=total_interests, total_users= total_users)
+        return (render_template('user.html',
+                users=users,
+                data=data,
+                user_name=user_name,
+                total_interests=total_interests(users),
+                total_users= total_users
+                ))
     else:
         return redirect(url_for('users_list'))
 
