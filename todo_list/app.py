cde50d63155cfc882d5ce0402c0f720b3febca43
Justin Schultz <SchultzJustin@ymail.com>
2025-07-14 20:30:51 +0000

    Refactor input validation

--- todo_list/app.py
+++ todo_list/app.py
@@ -8,7 +8,7 @@ from flask import (
                     session,
                     url_for,
                     )
-
+from todos.utils import error_for_list_title
 
 app = Flask(__name__)
 app.secret_key='secret1'
@@ -34,26 +34,21 @@ def get_lists():
 def create_list():
     title = request.form['list_title'].strip()
 
-    if any(lst['title'] == title for lst in session['lists']):
-        flash('A todo with this title already exists.', 'error')
-
+    error = error_for_list_title(title, session['lists'])
+    if error:
+        flash(error, 'error')
         return render_template('new_list.html', title=title)
 
-    if 1 <= len(title) <= 100:
-        session['lists'].append({
-            'id': str(uuid4()),
-            'title': title,
-            'todos': [],
-        })
+    session['lists'].append({
+        'id': str(uuid4()),
+        'title': title,
+        'todos': [],
+    })
 
-        flash('The list has been created.', 'success')
-        session.modified = True
+    flash('The list has been created.', 'success')
+    session.modified = True
 
-        return redirect(url_for('get_lists'))
-
-    flash('Todo titles must be between 1 and 100 characters.', 'error')
-
-    return render_template('new_list.html')
+    return redirect(url_for('get_lists'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
